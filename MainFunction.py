25177271584eff4b99d9e9ad948a0da9baae9759
jhe15 <hejin688@yahoo.com>
2015-10-13 16:55:37 -0700

    Create MainFunction.py

---
+++ MainFunction.py
@@ -0,0 +1,10 @@
+import US07
+import US27
+
+
+if __name__=='__main__':
+    print 'User Case 07 : Age Less than 150'
+    US07.AgeLessThan150()
+    print 'User Case 27: Current Age for each indiviuals' 
+    US27.CurrentAge()
+    
